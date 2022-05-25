9ffd0648be264d8232894c2619e791bdaf6a9790
krodox <julian@krodox.com>
2022-05-25 14:39:47 -0400

    kg to stones

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -2,7 +2,12 @@
 
 use Krodox\WeightConversions\Weight;
 
-it('can convert kilograms to pound correctly', function () {
+it('can convert kilograms to pounds correctly', function () {
     $pounds = Weight::kilograms(100)->toPounds();
     expect($pounds)->toEqual(220.46);
+});
+
+it('can convert kilograms to stones correctly', function () {
+    $pounds = Weight::kilograms(100)->toStones();
+    expect($pounds)->toEqual(15.747);
 });
